f8f9989f334533f2af9e5500eb310f95e4db68cf
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-06-25 15:47:20 -0400

    Time: 87 ms (37.36%), Space: 16.4 MB (43.88%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,11 +4,18 @@ class Solution:
         left = 0
         maximumLength = 0
 
-        for i in range(len(s)):
-            seen[s[i]] += 1
+        # for i in range(len(s)):
+        #     seen[s[i]] += 1
 
-            while(seen[s[i]] > 1):
-                seen[s[left]] -= 1
-                left += 1
-            maximumLength = max(maximumLength,i-left+1)
-        return maximumLength
+        #     while(seen[s[i]] > 1):
+        #         seen[s[left]] -= 1
+        #         left += 1
+        #     maximumLength = max(maximumLength,i-left+1)
+        # return maximumLength
+
+        for right in range(len(s)):
+            if s[right] in seen and seen[s[right]] + 1 > left:
+                left = seen[s[right]] + 1
+            seen[s[right]] = right
+            maximumLength = max(maximumLength,right-left+1)
+        return maximumLength 
