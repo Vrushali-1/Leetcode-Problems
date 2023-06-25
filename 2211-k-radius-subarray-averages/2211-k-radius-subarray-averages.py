d3ee0d45639a2e688cc340f7c8f95d077356dd18
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-06-25 15:19:29 -0400

    Time: 1627 ms (58.92%), Space: 34.8 MB (50.21%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
@@ -1,23 +1,26 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
 
-        n = len(nums)
-        answer = [-1] * n
-        windowSum = 0
+        length = len(nums)
+        addition = 0
 
-        if  k == 0:
+        averages = [-1 for i in range(length)]
+
+        if k == 0:
             return nums
         
-        if 2 * k + 1 > n:
-            return answer
+        if (2 * k + 1) > length:
+            return averages
+            
+        for i in range((2 * k + 1)):
+            addition += nums[i]
+        averages[k] = addition//(2 * k + 1)
 
-        for i in range(2 * k + 1):
-            windowSum += nums[i]
-        answer[k] = windowSum//(2*k+1)
-        for i in range(2*k+1,len(nums)):
-            windowSum = windowSum - nums[i-(2*k+1)]  + nums[i]
-            answer[i-k] = windowSum//(2*k+1)
-        return answer
+        for i in range((2 * k + 1),length):
+            addition = addition + nums[i] - nums [i - (2 * k + 1)]
+            averages[i-k] = addition//(2 * k + 1)
+        return averages
+
 
         
 
