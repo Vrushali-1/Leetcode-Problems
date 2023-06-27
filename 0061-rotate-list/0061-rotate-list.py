428824f4564127a2318f19fe24f1b978079b3002
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-06-27 15:23:05 -0400

    Time: 57 ms (34.12%), Space: 16.4 MB (49.19%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -5,8 +5,31 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # current = head
-        # n = 1   
+        current = head
+        n = 1   
+
+        # base cases
+        if not head:
+            return None
+        if not head.next:
+            return head
+        if k == 0:
+            return head
+        
+        while current.next:
+            current = current.next 
+            n += 1
+            
+        current.next = head
+        print(n)
+        new_tail = head
+        for i in range( n-k%n - 1):
+            new_tail = new_tail.next
+        new_head = new_tail.next
+
+        new_tail.next = None
+
+        return new_head if k > 0 else head
 
         # # base cases
         # if not head:
@@ -14,42 +37,22 @@ class Solution:
         # if not head.next:
         #     return head
         
-        # while current.next:
+        # # close the linked list into the ring
+        # old_tail = head
+        # n = 1
+        # while old_tail.next:
+        #     old_tail = old_tail.next
         #     n += 1
-        #     current = current.next 
-        # current.next = head
-
+        # old_tail.next = head
+        
+        # # find new tail : (n - k % n - 1)th node
+        # # and new head : (n - k % n)th node
         # new_tail = head
-        # for i in range( (n-k)%(n) - 1):
+        # for i in range(n - k % n - 1):
         #     new_tail = new_tail.next
         # new_head = new_tail.next
-
+        
+        # # break the ring
         # new_tail.next = None
-
+        
         # return new_head
-
-        # base cases
-        if not head:
-            return None
-        if not head.next:
-            return head
-        
-        # close the linked list into the ring
-        old_tail = head
-        n = 1
-        while old_tail.next:
-            old_tail = old_tail.next
-            n += 1
-        old_tail.next = head
-        
-        # find new tail : (n - k % n - 1)th node
-        # and new head : (n - k % n)th node
-        new_tail = head
-        for i in range(n - k % n - 1):
-            new_tail = new_tail.next
-        new_head = new_tail.next
-        
-        # break the ring
-        new_tail.next = None
-        
-        return new_head
