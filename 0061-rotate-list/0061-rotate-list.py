fadd1e98d07468f0cbeb1f05a69ca1e3573d4c74
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-06-27 15:34:32 -0400

    Time: 51 ms (69.27%), Space: 16.4 MB (12.84%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -7,29 +7,30 @@ class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         current = head
         n = 1   
-
-        # base cases
+         
+        #base case
         if not head:
             return None
         if not head.next:
             return head
-        if k == 0:
-            return head
-        
+
         while current.next:
             current = current.next 
             n += 1
-        k = k % n
+        k = k % n #actually how many times to rotate
+
+        # base cases
         if k == 0:
             return head
+      
             
-        current.next = head
-        print(n)
-        new_tail = head
-        for i in range( n - k - 1):
+        current.next = head #connect current to current head to make a ring
+
+        new_tail = head #again start new tail from previous head
+        for i in range( n - k - 1): #new tail will be n-k-1th node
             new_tail = new_tail.next
-        new_head = new_tail.next
+        new_head = new_tail.next #assign new head
 
-        new_tail.next = None
+        new_tail.next = None #break the ring
 
-        return new_head if k > 0 else head
+        return new_head 
