777c3e72e5351bfc111ce677a979d44058098711
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-07-04 14:27:49 -0400

    Time: 63 ms (6.77%), Space: 16.1 MB (97.03%) - LeetHub

---
+++ 1666-make-the-string-great/1666-make-the-string-great.py
@@ -0,0 +1,19 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+        
+        for i in range(len(s)):
+            if i == 0:
+                stack.append(s[i])
+            elif stack and s[i] != stack[-1]:
+                if s[i].lower() == stack[-1].lower():
+                    stack.pop()
+                else:
+                    stack.append(s[i])
+            else:
+                stack.append(s[i])
+                
+        return "".join(stack) if stack else ""
+                    
+                
+        
