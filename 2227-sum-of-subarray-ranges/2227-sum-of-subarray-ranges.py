4844279d6bac659e10ab5baa6ed78682d12c8a6e
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-07-06 19:34:09 -0400

    Time: 100 ms (88.69%), Space: 16.4 MB (96.43%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.py
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.py
@@ -4,20 +4,22 @@ class Solution:
         answer = 0
         n = len(nums)
 
-        for right in range(n + 1):
-            while stack and ( right == n or nums[stack[-1]] >= nums[right]):
-                mid = stack.pop()
-                left = -1 if not stack else stack[-1]
-                answer -= nums[mid] * (mid - left) * (right - mid)
-            stack.append(right) 
-
-        stack.clear()
-
+        #for loop to subtract all maximum values
         for right in range(n + 1):
             while stack and ( right == n or nums[stack[-1]] <= nums[right]):
                 mid = stack.pop()
                 left = -1 if not stack else stack[-1]
                 answer += nums[mid] * (mid - left) * (right - mid)
             stack.append(right)
-        
+
+        stack.clear()
+
+        # for loop to subtract of all minimum values
+        for right in range(n + 1):
+            while stack and ( right == n or nums[stack[-1]] >= nums[right]):
+                mid = stack.pop()
+                left = -1 if not stack else stack[-1]
+                answer -= nums[mid] * (mid - left) * (right - mid)
+            stack.append(right)     
+
         return answer
