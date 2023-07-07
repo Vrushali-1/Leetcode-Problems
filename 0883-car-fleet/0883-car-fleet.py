ed15d4b2442ccccabc5908cd2c56a3a645bdca93
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-07-07 11:50:11 -0400

    Time: 981 ms (42.76%), Space: 36.4 MB (76.65%) - LeetHub

---
+++ 0883-car-fleet/0883-car-fleet.py
@@ -0,0 +1,15 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = []
+
+        pair = [[p,s] for p,s in zip(position,speed)]
+
+        for i,c in sorted((pair))[::-1]:
+            distance = target - i
+            time = distance/c
+            if stack and time <= stack[-1]:
+                continue
+            else:
+                stack.append(time)
+        return len(stack)
+            
