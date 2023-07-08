9bd31be42f09501c8e1b9d7a666284c56beccf33
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-07-08 15:19:54 -0400

    Time: 1014 ms (10.38%), Space: 33.7 MB (94.22%) - LeetHub

--- 2338-minimum-consecutive-cards-to-pick-up/2338-minimum-consecutive-cards-to-pick-up.py
+++ 2338-minimum-consecutive-cards-to-pick-up/2338-minimum-consecutive-cards-to-pick-up.py
@@ -5,11 +5,8 @@ class Solution:
         left = 0
         for right in range(len(cards)):
             seen[cards[right]] += 1
-            if seen[cards[right]] > 1 :
-                while seen[cards[right]] > 1:
-                    answer = min(answer,right - left + 1)
-                    seen[cards[left]] -= 1
-                    # if seen[cards[left]] == 0:
-                    #     del seen[cards[left]]
-                    left += 1
+            while seen[cards[right]] > 1:
+                answer = min(answer,right - left + 1)
+                seen[cards[left]] -= 1
+                left += 1
         return answer if answer != float('inf') else -1
