91a306eb412c9c7e2cbb0dfd96f3eba1f0ef9398
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-07-09 15:12:35 -0400

    Time: 93 ms (53.87%), Space: 16.6 MB (22.96%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,37 +1,45 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
 
-        if len(s1) > len(s2): return False
+        s1Count = [0 for i in range(26)]
+        s2Count = [0 for i in range(26)]
 
-        s1Characters = [0 for i in range(26)]
-        s2Characters = [0 for i in range(26)]
+        matchCount = left = 0
 
-        matchCharacters = 0
-
+        if len(s1) > len(s2) or not s2:
+            return False
+        
         for i in range(len(s1)):
-            s1Characters[ord(s1[i]) - ord('a')] += 1
-            s2Characters[ord(s2[i]) - ord('a')] += 1
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
         
         for i in range(26):
-            if s1Characters[i] == s2Characters[i]:
-                matchCharacters += 1
+            if s1Count[i] == s2Count[i]:
+                matchCount += 1
         
-        left = 0
-        for right in range(len(s1),len(s2)):
-            if matchCharacters == 26: return True
+        if matchCount == 26:
+            return True
 
-            index = ord(s2[right]) - ord('a')
-            s2Characters[index] += 1
-            if s1Characters[index] == s2Characters[index]:
-                matchCharacters += 1
-            elif s2Characters[index] - 1 == s1Characters[index]:
-                matchCharacters -= 1
+        for right in range(len(s1),len(s2)):
+            if matchCount == 26:
+                return True
+
+            index = ord(s2[right]) - ord('a') #index of new right ele
+            s2Count[index] += 1
+            if s2Count[index] == s1Count[index]:
+                matchCount += 1
+            elif s2Count[index] - 1 == s1Count[index]:
+                matchCount -= 1
             
-            index = ord(s2[left]) - ord('a')
-            s2Characters[index] -= 1
-            if s1Characters[index] == s2Characters[index]:
-                matchCharacters += 1
-            elif s2Characters[index] + 1 == s1Characters[index]:
-                matchCharacters -= 1
-            left += 1
-        return matchCharacters == 26
+            index = ord(s2[left]) - ord('a') #index of the ele to be removed
+            s2Count[index] -= 1
+            if s2Count[index] == s1Count[index]:
+                matchCount += 1
+            elif s2Count[index] + 1 == s1Count[index]:
+                matchCount -= 1
+
+            left += 1 #increment left
+
+        return matchCount == 26
+
+        
