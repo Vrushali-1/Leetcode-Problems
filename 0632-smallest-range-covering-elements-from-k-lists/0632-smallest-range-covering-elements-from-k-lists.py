9814b35da0155b7241da4c0dfe4b1bd250302717
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-07-25 16:00:28 -0400

    Time: 243 ms (55.93%), Space: 22.6 MB (52.59%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -1,43 +1,36 @@
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
-        # n = len(nums)
-        # element = [0 for _ in range(n)]
-        # minheap = [[sequence[0],index] for sequence, index in enumerate(nums)]
-        # answer = []
-
-        # currentMin = min(pair[0] for pair in minheap)
-        # currentMax = max(pair[0] for pair in minheap)
-
-        # answer = [currentMin,currentMax]
-
-        # if currentMin == currentMax:
-        #     return answer
-        
-        # minheap.heapify()
-
         n = len(nums)
-        curr_idx = [0 for _ in range(n)]
-        heap = [[n[0], idx] for idx, n in enumerate(nums)]
+        elements = [0 for _ in range(n)]
+        minheap = [[sequence[0],index] for index,sequence in enumerate(nums)]
+        answer = []
 
-        curr_min = min([h[0] for h in heap])
-        curr_max = max([h[0] for h in heap])
-        res = [curr_min, curr_max]
-        if curr_min == curr_max: return res
-        # print(curr_min, curr_max)
-        heapq.heapify(heap)
-        while True:
-            _, idx = heapq.heappop(heap)
-            curr_idx[idx] += 1
-            if curr_idx[idx] == len(nums[idx]):
-                return res
-            if nums[idx][curr_idx[idx]] > curr_max:
-                curr_max = nums[idx][curr_idx[idx]]
-            heapq.heappush(heap, [nums[idx][curr_idx[idx]], idx])
-            curr_min = heap[0][0]
-            
-            if curr_max - curr_min < res[1] - res[0]:
-                res = [curr_min, curr_max]
-            if curr_min == curr_max:
-                return res
+        currentMin = min(pair[0] for pair in minheap)
+        currentMax = max(pair[0] for pair in minheap)
+
+        answer = [currentMin,currentMax]
+
+        if currentMin == currentMax:
+            return answer
         
+        heapq.heapify(minheap)
 
+        while True:
+            _,index = heapq.heappop(minheap)
+
+            elements[index] += 1
+
+            if elements[index] == len(nums[index]):
+                return answer
+            
+            if nums[index][elements[index]] > currentMax:
+                currentMax = nums[index][elements[index]]
+
+            heapq.heappush(minheap, [nums[index][elements[index]],index])
+            currentMin = minheap[0][0]
+
+            if currentMax - currentMin < answer[1] - answer[0]:
+                answer = [currentMin,currentMax]
+
+            if currentMin == currentMax:
+                return answer
