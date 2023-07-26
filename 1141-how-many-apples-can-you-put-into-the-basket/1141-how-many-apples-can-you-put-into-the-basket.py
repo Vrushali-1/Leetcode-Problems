afe84446265ca60079a20466f8cf93e725baa2a8
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-07-26 16:17:16 -0400

    Time: 128 ms (15.3%), Space: 16.6 MB (37.93%) - LeetHub

---
+++ 1141-how-many-apples-can-you-put-into-the-basket/1141-how-many-apples-can-you-put-into-the-basket.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxNumberOfApples(self, weight: List[int]) -> int:
+        total, i, answer = 0, 0, 0
+        weight.sort()
+        
+        while i < len(weight) and weight[i] + total <= 5000:
+            total += weight[i]
+            i += 1
+        return i
+        
