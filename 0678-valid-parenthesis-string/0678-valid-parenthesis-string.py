cccdb0d95269c150c00e81c4534045589f696d7b
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-07-31 23:44:44 -0400

    Time: 35 ms (96.56%), Space: 16.3 MB (45.9%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        low, high = 0,0
+
+        for char in(s):
+            low += 1 if char == '(' else -1
+            high += 1 if char != ')' else -1
+
+            if high < 0: break
+            low = max(low,0)
+
+        return low == 0 
