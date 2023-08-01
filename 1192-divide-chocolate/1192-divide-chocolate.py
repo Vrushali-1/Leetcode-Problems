523eb93049770f76970e2658ca074cd66348adf5
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-07-31 20:41:44 -0400

    Time: 185 ms (98.19%), Space: 18 MB (54.79%) - LeetHub

---
+++ 1192-divide-chocolate/1192-divide-chocolate.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
+        def check(minSweet):
+            currentSweetness = 0
+            people = 0
+            for sweet in sweetness:
+                currentSweetness += sweet
+
+                if currentSweetness >= minSweet:
+                    people += 1
+                    currentSweetness = 0
+            return people >= k + 1
+        
+        left, right = min(sweetness), sum(sweetness) // (k + 1)
+
+        while left < right:
+            mid = (left + right + 1) // 2
+
+            if check(mid):
+                left = mid
+            else:
+                right = mid - 1
+        return right
+
+
+        
