35bf8822046cfa914f1e084792d0beb29eda9faa
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-08-02 11:13:41 -0400

    Time: 850 ms (57.83%), Space: 18.6 MB (60.24%) - LeetHub

--- 2344-escape-the-spreading-fire/2344-escape-the-spreading-fire.py
+++ 2344-escape-the-spreading-fire/2344-escape-the-spreading-fire.py
@@ -1,53 +1,48 @@
 class Solution:
-	def maximumMinutes(self, grid: List[List[int]]) -> int:
-		mx = float('inf')
-		m,n = len(grid), len(grid[0])
-		arr = [[mx for i in range(n)] for j in range(m)]
-		q = deque([(j,i) for i in range(n) for j in range(m) if grid[j][i] == 1])
-		for i,j in q:
-			arr[i][j] = 0
-		visited = set(q)
-		temp = 0
-		while q:
-			l = len(q)
-			for j in range(l):
-				x,y = q.popleft()
-				visited.add((x,y))
-				arr[x][y] = temp
-				for i,j in (1,0),(0,1),(-1,0),(0,-1):
-					xx = x+i
-					yy = y+j
-					if 0<=xx<m and 0<=yy<n and grid[xx][yy] == 0 and (xx,yy) not in visited:
-						q.append((xx,yy))
-			temp += 1
-		def solve(x,y,temp):
-			nonlocal visited
-			val = arr[x][y]
-			if x == m-1 and y == n-1:
-				return temp <= val
-			visited.add((x,y))
-			if val <= temp:return False
-			for i,j in (1,0),(0,1),(-1,0),(0,-1):
-				xx = x+i
-				yy = y+j
-				if 0<=xx<m and 0<=yy<n and (xx,yy) not in visited and grid[xx][yy] != 2:
-					if solve(xx,yy, temp+1):
-						return True
-			# visited.remove((x,y))
-			return False
-
-		# for row in arr:
-		#     print(*row)
-		visited = set()
-		if not solve(0,0,0):return -1
-		l,r = 0,10**9
-		ans = None
-		while l <= r:
-			mid = (l+r)//2
-			visited = set()
-			if solve(0,0,mid):
-				ans = mid
-				l = mid+1
-			else:
-				r = mid-1
-		return ans
+    def maximumMinutes(self, grid: List[List[int]]) -> int:
+        R, C = len(grid), len(grid[0])
+        lo = 0
+        hi = 1000000000
+        fire = [[float('inf')] * C for _ in range(R)]
+        q = deque()
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] == 1:
+                    fire[r][c] = 0
+                    q.appendleft((r, c))
+        step = 0
+        while q:
+            nq = deque()
+            while q:
+                r, c = q.pop() 
+                for dr, dc in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+                    rr, cc = r + dr, c + dc
+                    if 0 <= rr < R and 0 <= cc < C and grid[rr][cc] == 0 and fire[rr][cc] == float('inf'):
+                        nq.appendleft((rr, cc))
+                        fire[rr][cc] = min(fire[rr][cc], step + 1)
+            step += 1
+            q = nq
+        def check(mid):
+            q = deque([(0, 0)])
+            v = {(0, 0): mid} 
+            while q:
+                r, c = q.pop()
+                step = v[r, c] 
+                if r == R - 1 and c == C - 1:
+                    return True
+                for dr, dc in  [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+                    rr, cc = dr + r, c + dc
+                    if 0 <= rr < R and 0 <= cc < C and grid[rr][cc] == 0 and (rr, cc) not in v and \
+                        (step + 1 < fire[rr][cc] or (rr, cc) == (R - 1, C - 1) and step + 1 <= fire[rr][cc] ):
+                        q.appendleft((rr, cc))
+                        v[rr, cc] = step + 1
+            return False
+        while lo < hi:
+            mid = (lo + hi + 1) // 2
+            if check(mid):
+                lo = mid
+            else:
+                hi = mid - 1
+        if check(lo):
+            return lo 
+        return -1
