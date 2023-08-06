3ae68053684522ca7e266cee6b551d257c9bcbed
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-08-06 19:54:34 -0400

    Time: 185 ms (11.87%), Space: 16.9 MB (69.38%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -1,24 +1,29 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        def check(time_limit):
-            stack = [(0, 0)]
-            visited = {(0, 0)}
+        def isValid(row,col):
+            return 0 <= row < n and 0 <= col < n
+        
+        def check(height):
+            stack = [(0,0)]
+            visited = set()
+            visited.add((0,0))
 
             while stack:
-                row, col = stack.pop()
+                row,col = stack.pop()
 
-                if row == col == n - 1:
+                if row == n - 1 and col == n - 1:
                     return True
-                    
-                for nrow, ncol in ((row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)):
-                    if (0 <= nrow < n and 0 <= ncol < n
-                            and (nrow, ncol) not in visited and grid[nrow][ncol] <= time_limit):
-                        stack.append((nrow, ncol))
-                        visited.add((nrow, ncol))
-            return False
 
+                for dx,dy in directions:
+                    r, c = row + dy, col + dx
+                    if isValid(r,c) and (r,c) not in visited and grid[r][c] <= height:
+                        stack.append((r,c))
+                        visited.add((r,c))
+            return False
+        
         n = len(grid)
         left,right = grid[0][0], n * n
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
 
         while left < right:
             mid = (left + right) // 2
