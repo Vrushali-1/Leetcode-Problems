4b459ca10534950885a8895f254b628490f4ccc5
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-08-07 19:50:01 -0400

    Time: 42 ms (95.18%), Space: 16.7 MB (29.24%) - LeetHub

---
+++ 1007-numbers-with-same-consecutive-differences/1007-numbers-with-same-consecutive-differences.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+
+        if n == 1:
+            return [i for i in range(0,10)]
+        def dfs(currentNum,N):
+            if N == 0:
+                answer.append(currentNum)
+                return
+            
+            tail = currentNum % 10
+            digits = set([tail + k, tail - k])
+
+            for digit in digits:
+                if  0 <= digit < 10:
+                    newNum = currentNum * 10 + digit
+                    dfs(newNum, N-1)
+        
+        answer = []
+        for num in range(1,10):
+            dfs(num,n-1)
+        return answer
