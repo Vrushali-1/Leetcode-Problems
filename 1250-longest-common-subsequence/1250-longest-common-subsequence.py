556c11c1585a41b4969976239b8878c4c5b1a7d9
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-09-06 15:09:00 -0400

    Time: 1923 ms (5.96%), Space: 504 MB (5.23%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -4,11 +4,16 @@ class Solution:
         def dp(i,j):
             if i == len(text1) or j == len(text2):
                 return 0
-            
+
             if text1[i] == text2[j]:
-                return 1 + dp(i+1,j+1)
+                memo[(i,j)] = 1 + dp(i+1,j+1)
             
-            return max(dp(i+1,j),dp(i,j+1))
-        #memo = {}
+            if (i,j) in memo:
+                return memo[(i,j)] 
+            
+            memo[(i,j)] = max(dp(i+1,j),dp(i,j+1))
+
+            return memo[(i,j)]
+        memo = {}
         return dp(0,0)
         
