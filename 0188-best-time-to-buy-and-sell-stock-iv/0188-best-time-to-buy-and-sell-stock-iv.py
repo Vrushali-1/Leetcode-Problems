d957b8d0e823e0b76916a0b51e58d53a7524f54d
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-10-23 16:11:33 -0400

    Time: 341 ms (13.15%), Space: 80.7 MB (20.34%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,42 +1,23 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        # @cache
-        # def dp(day,holding,remain):
-        #     if day == len(prices) or remain == 0:
-        #         return 0
-            
-        #     ans = dp(day+1,holding,remain)
-        #     if holding:
-        #         ans = max(ans,prices[day] + dp(day+1,False,remain - 1))
-
-        #     else:
-        #         ans = max(ans,-prices[day] + dp(day+1,True,remain))
-            
-        #     return ans
-        
-        # #memo = {}
-        # return dp(0,False,k)
-        memo = {}  # Create a memoization dictionary to store computed results.
-
-        def dp(day, holding, remain):
-            # Check if the result for this state is already memoized.
-            if (day, holding, remain) in memo:
-                return memo[(day, holding, remain)]
-
+        def dp(day,holding,remain):
             if day == len(prices) or remain == 0:
                 return 0
+            
+            if (day,holding,remain) in memo:
+                return memo[(day,holding,remain)]
 
-            ans = dp(day + 1, holding, remain)
+            ans = dp(day+1,holding,remain)
             if holding:
-                ans = max(ans, prices[day] + dp(day + 1, False, remain - 1))
+                ans = max(ans,prices[day] + dp(day+1,False,remain - 1))
+
             else:
-                ans = max(ans, -prices[day] + dp(day + 1, True, remain))
-
-            # Memoize the result for this state.
-            memo[(day, holding, remain)] = ans
-
+                ans = max(ans,-prices[day] + dp(day+1,True,remain))
+            
+            memo[(day,holding,remain)] = ans
             return ans
-
-        return dp(0, False, k)
+        
+        memo = {}
+        return dp(0,False,k)
 
         
