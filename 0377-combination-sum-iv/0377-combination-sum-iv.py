e180feb4a5694cf2b3c010f0641060625725ecb2
Vrushali Patil <60639609+Vrushali-1@users.noreply.github.com>
2023-10-31 21:21:27 -0400

    Time: 53 ms (14.61%), Space: 16.2 MB (80.38%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,20 +1,35 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
             
-        @functools.lru_cache(maxsize = None)
-        def combs(remain):
-            if remain == 0:
-                return 1
+        # @functools.lru_cache(maxsize = None)
+        # def combs(remain):
+        #     if remain == 0:
+        #         return 1
 
-            result = 0
+        #     result = 0
+        #     for num in nums:
+        #         if remain - num >= 0:
+        #             result += combs(remain - num)
+        #         # potential optimization
+        #         # else:
+        #         #     break
+
+        #     return result
+
+        # return combs(target)
+
+            # minor optimization
+            # nums.sort()
+        dp = [0 for i in range(target+1)]
+        dp[0] = 1
+
+        for comb_sum in range(target+1):
+
             for num in nums:
-                if remain - num >= 0:
-                    result += combs(remain - num)
-                # potential optimization
+                if comb_sum - num >= 0:
+                    dp[comb_sum] += dp[comb_sum-num]
+                # minor optimization, early stopping.
                 # else:
-                #     break
-
-            return result
-
-        return combs(target)
+                #    break
+        return dp[target]
         
